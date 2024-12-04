a5a0a5957ebe6ba3750d790165ffad1cb5b1e26c
Ebrahim Aboelkhair <ebrahimaboelkhair10@gmail.com>
2024-12-04 19:54:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img')
+let container = document.querySelector('.container')
+
+
+function phones(phone) {
+    img.src = phone;;
+
+}
+function colores(colore) {
+    container.style.background = colore;
+}
